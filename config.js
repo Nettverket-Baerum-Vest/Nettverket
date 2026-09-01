d3ee49be2f590ba8eaa185ef269cfcd4285f83db
benbarrios87 <benbarrios87@gmail.com>
2026-09-01 13:13:16 +0200

    Koble til Supabase-prosjektet

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Fyll inn disse to etter at Supabase-prosjektet er opprettet.
 // Finnes under: Project settings -> API i Supabase-dashbordet.
-const SUPABASE_URL = "SETT_INN_SUPABASE_URL_HER";
-const SUPABASE_KEY = "SETT_INN_SUPABASE_ANON_KEY_HER";
+const SUPABASE_URL = "https://mughiohvvtfdqxtroutu.supabase.co";
+const SUPABASE_KEY = "sb_publishable_z4k6Y5YdaIvo-gWQJjfUSg_1tAd1v0w";
